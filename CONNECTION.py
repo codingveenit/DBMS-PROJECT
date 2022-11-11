49c08a0738fe2337c15d78d5c3d8982020057fd7
codingveenit <116004094+codingveenit@users.noreply.github.com>
2022-11-11 23:38:32 +0530

    Delete CONNECTION.py

--- CONNECTION.py
+++
@@ -1,9 +0,0 @@
-def connection():
-    import mysql.connector
-    mycon = mysql.connector.connect(
-        host='localhost', user='root', passwd='', database='Bus')
-    if mycon.is_connected():
-        print("successfully connected")
-
-
-connection()
